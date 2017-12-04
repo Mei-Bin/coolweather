14970d80e0fa2dfc2e17a8dcbdad7c0510d0ad4d
meibin <531618983@qq.com>
2017-12-05 07:20:43 +0800

    完成便利省市县三级列表功能

--- app/src/main/java/service/coolweather/MainActivity.java
+++ app/src/main/java/service/coolweather/MainActivity.java
@@ -1,13 +1,47 @@
 package service.coolweather;
 
+import android.os.Build;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import service.coolweather.util.LogUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean isExit = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (!isExit) {
+                    isExit = true;
+                    Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            isExit = false;
+                        }
+                    }, 2000);
+                }else {
+                    finish();
+                }
+                return true; //表示已处理
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ app/src/main/java/service/coolweather/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package service.coolweather.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by meibin on 2017/12/3.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
+
